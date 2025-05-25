989a827edba2a6156edf34dd4ae3c6a588eb8c57
Mahesh Bokkisam <Mahesh.bokkisam@cognizant.com>
2025-05-25 13:29:41 +0530

    w3 school template tryw3css_templates_gourmet_catering.htm

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,9 +1,21 @@
 export default function decorate(block) {
+  console.log("block ", block.parentNode.parentNode.classList);
+
+  let rowIds = [];
+  if(block.parentNode.parentNode.classList.contains('columns-container')) {
+    const attrValues = block.parentNode.parentNode.getAttribute('data-row-ids').split(',');
+    attrValues && attrValues.length && attrValues.forEach((id) => {
+      rowIds.push(id.trim());
+    }) 
+  }
+
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
   // setup image columns
-  [...block.children].forEach((row) => {
+  [...block.children].forEach((row, index) => {
+    console.log("ROW ", row);
+    row.setAttribute('id', rowIds[index]);
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
       if (pic) {
