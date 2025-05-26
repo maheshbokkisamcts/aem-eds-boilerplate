957cb00ea315e4d6769ed739dbff0f45e496cee4
Mahesh Bokkisam <Mahesh.bokkisam@cognizant.com>
2025-05-26 13:08:12 +0530

    lint issues

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,11 +1,10 @@
 export default function decorate(block) {
-  console.log("block ", block.parentNode.parentNode.classList);
 
   // Fetch row ids, read it from metadata of document section/block
-  let rowIds = [];
-  if(block.parentNode.parentNode.classList.contains('columns-container')) {
+  const rowIds = [];
+  if (block.parentNode.parentNode.classList.contains('columns-container')) {
     const attrValues = block.parentNode.parentNode.getAttribute('data-row-ids').split(',');
-    attrValues && attrValues.length && attrValues.forEach((id) => {
+   attrValues?.length && attrValues.forEach((id) => {
       rowIds.push(id.trim());
     }) 
   }
@@ -15,9 +14,8 @@ export default function decorate(block) {
 
   // setup image columns
   [...block.children].forEach((row, index) => {
-    console.log("ROW ", row);
     // Add rowids to each row if available
-    rowIds && rowIds.length && rowIds[index] && row.setAttribute('id', rowIds[index]);
+    rowIds?.length && rowIds[index] && row.setAttribute('id', rowIds[index]);
 
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
